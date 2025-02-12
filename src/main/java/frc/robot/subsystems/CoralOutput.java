7dd585688b0e47c772f02783878a5951e5b3264a
ManuelCSW <mllavina2027@csw.org>
2025-02-12 15:56:25 -0500

    Manuel Llavina: Since it's going to be a solenoid, there's going to be two functions for turning on and off and stuff, and I added the object.

--- src/main/java/frc/robot/subsystems/CoralOutput.java
+++ src/main/java/frc/robot/subsystems/CoralOutput.java
@@ -6,43 +6,66 @@
 
 // imports, usually done automatically by vscode
 package frc.robot.subsystems;
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
+// This is the system that will output the coral on the bottom for a ranking point
+// Nicholas says: move wheels! DONE.
+// We'll make a method for turning on wheels to put the coral
+
+// THE ACTUAL WAY:
+// It's going to be a solenoid that's used for the auto ranking point
+// Two functions to turn it on and to turn it off.
+// Don't forget to define it as well.
+
+
+
+
 // class declaration and inheritance
-public class AutoOutput extends SubsystemBase {
+public class CoralOutput extends SubsystemBase {
 
     // create the physical objects that the subsytem uses
-    // motors, sensors,...
-    private SparkMax test_motor = new SparkMax(0, MotorType.kBrushless);
-    private DigitalInput test_input = new DigitalInput(0);
+    // THE SOLENOID
+    private Solenoid m_Solenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
+
+    
+    // What is a digital input:
+
 
 // constructor - will be run ONCE when this class, ExampleSubsystem, is instantiated
 // used to run code that only needs to be done once, such as setting presets or constants
 // must have exactly the same name as the class
 // also, this is where other subsystems are passed into this one, such as the drivebase for example
-  public AutoOutput(SwerveSubsystem drivebase) {
+  public CoralOutput(SwerveSubsystem drivebase) {
+
   }
 
-// demonstrative method that returns the state of the digital input
-  public boolean getInput() { 
-    return test_input.get();
+
+
+// A function to open up the Solenoid
+
+  public void openSolenoid(double speed) {
+    m_Solenoid.set(true);
   }
 
-// demonstrative method that sets the motor speed
-  public void setMotorSpeed(double speed) {
-    test_motor.set(speed);
+  public void closeSolenoid(double speed) {
+    m_Solenoid.set(false);
   }
+
 
   @Override
   public void periodic() {
     // WPILIB MUST HAVE - this method will be called once per scheduler run - 20ms
 
-    System.out.println(test_motor.getEncoder().getVelocity()); // print motor velocity
-    System.out.println(test_input.get()); // print state of DigitalInput
+    //System.out.println(coral_motor.getEncoder().getVelocity()); // print motor velocity
+    //System.out.println(coral_input.get()); // print state of DigitalInput
   }
 
   @Override
