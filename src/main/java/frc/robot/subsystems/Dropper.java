b4b8702105c359706134c5181864e3ffdc5db099
atbutler09 <atbutler09@outlook.com>
2025-02-12 16:20:42 -0500

    creates takeIn and takeOut

--- src/main/java/frc/robot/subsystems/Dropper.java
+++ src/main/java/frc/robot/subsystems/Dropper.java
@@ -8,6 +8,7 @@
 package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -18,28 +19,55 @@ public class Dropper extends SubsystemBase {
     // create the physical objects that the subsytem uses
     // motors, sensors,...
     private SparkMax dropper_motor = new SparkMax(0, MotorType.kBrushless);
+    private DigitalInput coralIn = new DigitalInput(0);
+
 
 // constructor - will be run ONCE when this class, ExampleSubsystem, is instantiated
 // used to run code that only needs to be done once, such as setting presets or constants
 // must have exactly the same name as the class
 // also, this is where other subsystems are passed into this one, such as the drivebase for example
   public Dropper(SwerveSubsystem drivebase) {
+    // constructor, doesnt have any presets (don't need right now)
+  }
 
+  // demonstrative method that returns the state of the digital input
+  public boolean getInput() { 
+    return coralIn.get();
   }
 
 // demonstrative method that sets the motor speed
   public void startMotors(double speed) {
     dropper_motor.set(speed);
-  }
+    }
+  
 
   public void stopMotors() {
    dropper_motor.set(0); 
+  }
+
+  public void takeIn() {
+    if (getInput() == false){
+      startMotors(.05);
+    }
+    if (getInput() == true) {
+      stopMotors();
+    }
+  }
+
+  public void pushOut() {
+    if (getInput() == true){
+      startMotors(.05);
+    if (getInput() == false) {
+      stopMotors();
+    }
+    }
   }
 
   @Override
   public void periodic() {
     // WPILIB MUST HAVE - this method will be called once per scheduler run - 20ms
     System.out.println(dropper_motor.getEncoder().getVelocity()); // print motor velocity
+    System.out.println(coralIn.get());
   }
 
   @Override
