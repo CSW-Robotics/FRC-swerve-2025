59f3b8a55aef16efd2ba970076a0c16d24c83510
Max Lawton <max@lawtonnation.com>
2025-03-14 21:27:39 -0400

    fixed die on elevaotror

--- src/main/java/frc/robot/commands/DieOnElevatorLevel.java
+++ src/main/java/frc/robot/commands/DieOnElevatorLevel.java
@@ -7,17 +7,17 @@ import frc.robot.subsystems.LimeLight;
 public class DieOnElevatorLevel extends Command{
 
     private final Elevator m_elevator;
-    private int target_stage;
+    private final int m_target_stage;
 
     public DieOnElevatorLevel(Elevator elevator, int target_stage) {
         m_elevator = elevator;
-        target_stage = target_stage;
+        m_target_stage = target_stage;
         addRequirements(m_elevator);
     }
 
     @Override
     public boolean isFinished() {
-        if (m_elevator.currentStage == target_stage) {
+        if (m_elevator.currentStage == m_target_stage) {
             return true;
         } else {
             return false;
