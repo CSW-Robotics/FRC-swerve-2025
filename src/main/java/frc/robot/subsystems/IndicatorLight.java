a4ac9a76a83deead2cf33aca2ce5f006ca11b805
Nicholas Palmer <npalmer2028@csw.org>
2025-03-17 12:14:42 -0400

    Updated the LED code

--- src/main/java/frc/robot/subsystems/IndicatorLight.java
+++ src/main/java/frc/robot/subsystems/IndicatorLight.java
@@ -2,26 +2,35 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LEDs;
 
 public class IndicatorLight extends SubsystemBase {
 
+    private final LEDs m_led;
+    private boolean m_led_previous = false;
     private final Elevator m_elevator;
     private final LimeLight m_limelight;
-    private final PowerDistribution m_pdh = new PowerDistribution(1, PowerDistribution.ModuleType.kRev);
 
-    public IndicatorLight(Elevator elevator, LimeLight limeLight) {
+    public IndicatorLight(LEDs led, Elevator elevator, LimeLight limeLight) {
+        m_led = led;
         m_elevator = elevator;
         m_limelight = limeLight;
+
     }
 
     @Override
     public void periodic(){
 
-        if (m_elevator.currentStage == 0 && m_limelight.DDDx3_data3D[2] < 0.5){
-            m_pdh.setSwitchableChannel(true);
-        } else{
-            m_pdh.setSwitchableChannel(false);
+        if (m_elevator.currentStage == 0 && m_limelight.DDDx3_data3D[2] < 0.5 && m_led_previous == false){
+           
+            m_led_previous = true;
+        } 
+        
+        else if (m_led_previous == true){
+
+            m_led_previous = false;
         }
+        
   
     }
 
